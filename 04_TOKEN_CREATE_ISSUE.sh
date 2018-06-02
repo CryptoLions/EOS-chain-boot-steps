974fc222140c73a7b1836ce6ddd97472b40ca9de
Bohdan Cryptolions <37701692+ansigroup@users.noreply.github.com>
2018-06-02 04:28:42 +0300

    Update 04_TOKEN_CREATE_ISSUE.sh

--- 04_TOKEN_CREATE_ISSUE.sh
+++ 04_TOKEN_CREATE_ISSUE.sh
@@ -11,6 +11,8 @@
 ./cleos.sh push action eosio.token create '["eosio", "10000000000.0000 EOS", 0, 0, 0]' -p eosio.token
 ./cleos.sh push action eosio.token issue '["eosio",  "1000000000.0000 EOS", "init"]' -p eosio
 
+./cleos.sh push action eosio.token create '["eosio", "1000000000.0000 JUNGLE", 0, 0, 0]' -p eosio.token
+./cleos.sh push action eosio.token issue '["eosio", "1000000000.0000 JUNGLE", "init"]' -p eosio
 
 
 
