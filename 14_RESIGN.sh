d81c6c0e8d55b9e17b8a1224a545c8f136e36335
Bohdan Cryptolions <37701692+ansigroup@users.noreply.github.com>
2018-06-02 04:26:14 +0300

    Update 14_RESIGN.sh

--- 14_RESIGN.sh
+++ 14_RESIGN.sh
@@ -1,1 +1,34 @@
+#!/bin/bash
+################################################################################
+#
+# Scrip Created by http://CryptoLions.io
+# For EOS Junlge testnet
+#
+# https://github.com/CryptoLions/
+#
+################################################################################
 
+EOSIO_PRODUCER_KEY="$( jq -r '.EOSIO_PRODUCER_PUB_KEY' "00_CONFIG.conf" )"
+SYSTEM_ACCOUNT=($( jq -r '.SYSTEM_ACCOUNT' "00_CONFIG.conf" ))
+
+
+
+
+account="eosio"
+controller="eosio.prods"
+push action eosio updateauth '{"account": "'$account'", "permission": "owner",  "parent": "",  "auth": { "threshold": 1, "keys": [], "waits": [], "accounts": [{ "weight": 1, "permission": {"actor": "'$controller'", "permission": "active"}
+ }] } } ' -p $account@owner
+push action eosio updateauth '{"account": "'$account'", "permission": "active",  "parent": "owner",  "auth": { "threshold": 1, "keys": [], "waits": [], "accounts": [{ "weight": 1, "permission": {"actor": "'$controller'", "permission": "ac
+tive"} }] } }' -p $account@active
+
+
+for sa in "${SYSTEM_ACCOUNT[@]}"
+do
+    account="$sa"
+    controller="eosio"
+    push action eosio updateauth '{"account": "'$account'", "permission": "owner",  "parent": "",  "auth": { "threshold": 1, "keys": [], "waits": [], "accounts": [{ "weight": 1, "permission": {"actor": "'$controller'", "permission": "acti
+ve"} }] } } ' -p $account@owner
+    push action eosio updateauth '{"account": "'$account'", "permission": "active",  "parent": "owner",  "auth": { "threshold": 1, "keys": [], "waits": [], "accounts": [{ "weight": 1, "permission": {"actor": "'$controller'", "permission":
+ "active"} }] } }' -p $account@active
+
+done
